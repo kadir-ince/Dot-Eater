23f8935381bbf6550266c8a40ba957a2c3e7568f
Kadir Ince <you.can.send@pm.me>
2021-01-13 19:25:53 +0300

    Add Game Files

---
+++ Assets/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    public float moveSpeed;
+    public float rotateAmount;
+    float rot;
+    int score;
+    public GameObject winText;
+
+
+    public Camera MainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            if (mousePos.x < 0)
+            {
+                rot = rotateAmount;
+            }
+            else
+            {
+                rot = -rotateAmount;
+            }
+            transform.Rotate(0, 0, rot);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rbody.velocity = transform.up * moveSpeed;
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Dot")
+        {
+            Destroy(collision.gameObject);
+            score++;
+            if (score >= 7)
+            {
+                winText.SetActive(true);
+                moveSpeed = 0;
+                transform.Rotate(0, 0, 9);
+            }
+        }
+        else if (collision.gameObject.tag == "Danger")
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+    }
+}
